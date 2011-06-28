4cdd7c6da94ab0ae72028ac82641255a7597c751
Bjarne Nørgaard <bjarne.norgaard@nokia.com>
2011-06-28 11:25:15 +0200

    line in the sand

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   #validation stuff:
   validates :title, :description, :image_url, :presence => true
@@ -8,6 +11,16 @@ class Product < ActiveRecord::Base
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
-
   }
+
+  private
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
