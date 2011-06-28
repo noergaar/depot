7dd9a6f1bdf244b62cce06951cf9a3bfe9487b3a
Bjarne Nørgaard <bjarne.norgaard@nokia.com>
2011-06-28 12:58:57 +0200

    End of chapter 9

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -44,6 +44,9 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     @line_item = @cart.line_items.build(:product => product)
 
+    #intialise counter
+    session[:counter]=0
+
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to(@line_item.cart, :notice => 'Line item was successfully created.')}
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,14 @@ class StoreController < ApplicationController
   def index
     @products = Product.all
     @time = Time.zone.now
+    @count = increment_count
+  end
+
+  def increment_count
+    if session[:counter].nil?
+      session[:counter]=0
+    end
+    session[:counter] += 1
   end
 
 end
