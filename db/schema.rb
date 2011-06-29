cbe98d94eabc9ffb18c1bf9b5aa7e45c1bf7426c
Bjarne Nørgaard <bjarne.norgaard@nokia.com>
2011-06-29 11:27:01 +0200

    security

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110624112458) do
+ActiveRecord::Schema.define(:version => 20110628115059) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -19,9 +19,10 @@ ActiveRecord::Schema.define(:version => 20110624112458) do
 
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
-    t.integer  "card_id"
+    t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
